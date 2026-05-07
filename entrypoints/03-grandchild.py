71977dd1b46615d6a0966610f91a4aeaeeacb308
Bruno Jambeiro <brunojambeirom@gmail.com>
2026-05-07 18:58:17 -0300

    adiciona 03-grandchild.py

--- entrypoints/03-grandchild.py
+++ entrypoints/03-grandchild.py
@@ -14,10 +14,32 @@ if __name__ == "__main__":
     renderer = urenderer.renderer.PyplotRenderer(1920, 1080)
     runtime = urenderer.application.Runtime(renderer, name="03-grandchild")
 
+    main_cube = urenderer.node.Node()
+
+    main_cube.translation = np.array([0, 0, -5], np.float64)
+    main_cube.rotation = np.array([45, 45, 90], np.float64)
+    main_cube.render_data = urenderer.geometry.polygonal_ifs.get_ifs_cube()
+    
+    cube_1 = urenderer.node.Node()
+    cube_1.translation = np.array([0, 1, 0], np.float64)
+    cube_1.rotation = np.array([0, 0, 0], np.float64)
+    cube_1.render_data = urenderer.geometry.polygonal_ifs.get_ifs_cube()
+    
+    cube_2 = urenderer.node.Node()
+
+    cube_2.translation = np.array([0, 1, 0], np.float64)
+    cube_2.rotation = np.array([0, 0, 0], np.float64)
+    cube_2.render_data = urenderer.geometry.polygonal_ifs.get_ifs_cube()
+    
+    cube_1.add_child(cube_2)
+    main_cube.add_child(cube_1)
+    runtime.scene.add_child(main_cube)
     # Crie a cena
 
     runtime.iter(capture=True)
-
+    
+    
+    main_cube.rotation = np.array([45, 45, 135], np.float64)
     # Rotacione o nó avô
 
     runtime.iter(capture=True)
